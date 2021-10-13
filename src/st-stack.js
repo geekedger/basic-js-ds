53b7fc521a1344370a8c42cccbe134a14cea53b1
geekedger <geekedger@gmail.com>
2021-10-13 16:18:30 +0300

    feat: solve smth

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ 
+constructor()
+{
+    this.items = [];
+}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+push(element) {
+  this.items.push(element);
+}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+pop() {
+      if (this.items.length == 0)
+      return "Underflow";
+  return this.items.pop();
+}
+
+peek() {
+      return this.items[this.items.length - 1];
   }
 }
